06b6239d33872767df0852f75665f20ed69bc691
numairmansur <numair.mansur@gmail.com>
2015-10-22 16:21:44 +0200

    Some more changes

--- inverted_index.py
+++ inverted_index.py
@@ -1,1 +1,26 @@
+'''
+Copyright 2015 University of Freibrug
+Hannah Bast <bast@cs.uni-freiburg.de>
 
+Edits for Exercise-1
+
+Numair Mansur <numair.mansur@gmail.com>
+'''
+
+import re
+
+''' A simple inverted index'''
+
+class InvertedIndex:
+
+	def read_from_file(self,file_name):
+		with open(file_name) as file:
+			for line in file:
+				for word in re.split("\W+", line):
+					word = word.lower()
+					print word
+
+
+if __name__ == "__main__":
+	ii = InvertedIndex()
+	ii.read_from_file("example.txt")
