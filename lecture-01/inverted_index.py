f476790cd875087f441c8a25fc2f3c5ee8dfc6a9
Muhammad Numair Mansur <mansurm@informatik.uni-freiburg.de>
2015-10-24 16:26:39 +0200

    changess

--- lecture-01/inverted_index.py
+++ lecture-01/inverted_index.py
@@ -33,9 +33,6 @@ class InvertedIndex:
                 for word in re.split("\W+", line):
                     word = word.lower()
                     if len(word) > 0:
-                    	if word == 'cool' and doc_id == 5327:
-                    		print "FOUND COOL !!! in DOC ID: " + str(doc_id)
-                    		print line
                         """ If a word is seen for first time, create an empty
                         inverted list for it. """
                         if word not in self.inverted_lists:
