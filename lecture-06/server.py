52cbf1b5f49b8e951f3b6679a41ba91c4301cf2a
anatskiy <evgeny.anatskiy@gmail.com>
2015-11-26 23:32:33 +0100

    did Exercise 6 on 95%

--- lecture-06/server.py
+++ lecture-06/server.py
@@ -10,13 +10,89 @@ import socket
 import time
 import sys
 import re
+import json
+from urllib.request import unquote
 
-# IMPORTANT: For the exercise sheet, please adhere to the following:
-#
-#  1.  The main program should be called server.py
-#  2.  It should take exactly TWO arguments: <file> <port>, where
-#      <file> is the full path to the file with the movie records,
-#      and <port> is the port on which the server is listening.
+from qgram_index import QgramIndex
+
+
+class Server:
+    """ :) """
+
+    def __init__(self, port):
+        self.port = port
+
+        # Create communication socket and listen on port <port>.
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind((socket.gethostname(), port))
+        self.server.listen(5)
+
+    def accept(self):
+        return self.server.accept()
+
+    def get_address(self):
+        return 'http://' + socket.gethostbyname(socket.gethostname()) + ':' + \
+               str(self.port) + '/'
+
+
+class Response:
+    """ :) """
+
+    def __init__(self):
+        self.content_type = 'text/plain'
+        self.content = ''
+
+    def set_content_type(self, file):
+        if file.endswith('.html'):
+            self.content_type = 'text/html'
+        elif file.endswith('.css'):
+            self.content_type = 'text/css'
+        elif file.endswith('.js'):
+            self.content_type = 'application/javascript'
+
+    def get_hits(self, qi, query):
+        normalized_query = re.sub('\W+', '', unquote(query)).lower()
+        delta = len(normalized_query) // 4
+
+        hits = qi.find_matches(normalized_query, delta)
+        result = [{'title': hit[0], 'year': hit[1]} for hit in hits]
+
+        return json.dumps(result)
+
+    def set_content(self, request, qi):
+        match = re.match(r'^GET /(.*) HTTP/1.1', request)
+        if match:
+            query = match.group(1)
+            if query == '':
+                query = 'index.html'
+
+            if not re.match(r'/', query):
+                if query.startswith('?q='):
+                    query = re.match(r'^\?q=(.*)', query).group(1)
+                    self.content = self.get_hits(qi, query)
+                else:
+                    # File is requested
+                    try:
+                        with open(query) as file:
+                            self.content = file.read()
+                            self.set_content_type(query)
+                    except FileNotFoundError:
+                        self.content = ''
+            else:
+                self.content = ''
+
+    def get_response(self):
+        if any(self.content):
+            content_length = len(self.content)
+            res = 'HTTP/1.1 200 OK\r\n' \
+                  'Content-Length: %d\r\n' \
+                  'Content-Type: %s\r\n' \
+                  '\r\n%s' % (content_length, self.content_type, self.content)
+        else:
+            res = 'HTTP/1.1 404 Not Found\r\n'
+        return res.encode('ascii')
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
@@ -26,14 +102,14 @@ if __name__ == '__main__':
     input_file = sys.argv[1]
     port = int(sys.argv[2])
 
-    # Create communication socket and listen on port <port>.
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((socket.gethostname(), port))
-    server.listen(5)
+    # Reading the movies file
+    print('Loading movies file...')
+    qi = QgramIndex(3)
+    qi.read_from_file(input_file)
 
-    print('Starting the server at http://%s:%s/' %
-          (socket.gethostbyname(socket.gethostname()), port))
+    # Create Server object
+    server = Server(port)
+    print('Starting the server at ' + server.get_address())
 
     # Server loop.
     while True:
@@ -45,45 +121,8 @@ if __name__ == '__main__':
               (time.strftime('%d/%b/%Y %H:%M:%S'),
                re.match(r'^GET (.*) HTTP/1.1', request).group(1)))
 
-        content_type = 'text/plain'
-
-        # Check ...
-        match = re.match(r'^GET /(.*) HTTP/1.1', request)
-        if match:
-            query = match.group(1)
-            if query == '':
-                query = 'index.html'
-
-            if not re.match(r'/', query):
-                if query.startswith('?q='):
-                    query = re.match(r'^\?q=(.*)', query).group(1)
-
-                    # TODO: use fuzzy prefix search here
-                    content = query
-                else:
-                    try:
-                        with open(query) as file:
-                            content = file.read()
-                            if query.endswith('.html'):
-                                content_type = 'text/html'
-                            elif query.endswith('.css'):
-                                content_type = 'text/css'
-                            elif query.endswith('.js'):
-                                content_type = 'application/javascript'
-                    except FileNotFoundError:
-                        content = ''
-            else:
-                content = ''
-
         # Send the response
-        if any(content):
-            content_length = len(content)
-            response = 'HTTP/1.1 200 OK\r\n' \
-                       'Content-Length: %d\r\n' \
-                       'Content-Type: %s\r\n' \
-                       '\r\n%s' % (content_length, content_type, content)
-        else:
-            response = 'HTTP/1.1 404 Not Found\r\n'
-
-        client.send(response.encode('ascii'))
+        response = Response()
+        response.set_content(request, qi)
+        client.send(response.get_response())
         client.close()
--- lecture-06/static/js/search.js
+++ lecture-06/static/js/search.js
@@ -3,15 +3,35 @@ $(function() {
         port = window.location.port;
 
     $('#search').keyup(function() {
+        var $resultArea = $('#result'),
+            $spinner = $('.spinner');
+
+        $resultArea.html('');
+        $spinner.show();
+
+        // TODO: filter key presses, don't take Shift, Ctrl, Cmd, Alt into
+        // account, otherwise, extra requests will be sent -> BAD performance
+
         var query = $(this).val(),
             url = 'http://' + host + ':' + port + '/?q=' + query;
 
         if (query != '') {
             $.get(url, function (result) {
-                $('#result').html('Result: ' + result);
+                if (result != '[]') {
+                    var movies = $.parseJSON(result), list = '';
+                    $(movies).each(function(idx, movie) {
+                        list += '<li>'+ movie.title + ' (' + movie.year + ')</li>';
+                    });
+                    $spinner.hide();
+                    $resultArea.html('<ul>' + list + '</ul>');
+                } else {
+                    $spinner.hide();
+                    $resultArea.html('No hits :(');
+                }
             });
         } else {
-            $('#result').html('');
+            $spinner.hide();
+            $resultArea.html('');
         }
     });
 });
