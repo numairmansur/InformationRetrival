92c981bbde84155bd7cfefaccc7bab01687fcaf1
anatskiy <evgeny.anatskiy@gmail.com>
2015-11-26 23:42:47 +0100

    96%

--- lecture-06/static/js/search.js
+++ lecture-06/static/js/search.js
@@ -2,36 +2,34 @@ $(function() {
     var host = window.location.hostname,
         port = window.location.port;
 
-    $('#search').keyup(function() {
+    $('#search').keyup(function(e) {
         var $resultArea = $('#result'),
-            $spinner = $('.spinner');
+            $spinner = $('.spinner'),
+            forbiddenKeyCodes = [16, 17, 18, 91, 13, 93, 37, 38, 39, 40];
 
-        $resultArea.html('');
-        $spinner.show();
+        if (forbiddenKeyCodes.indexOf(e.keyCode) == -1) {
+            var query = $(this).val(),
+                url = 'http://' + host + ':' + port + '/?q=' + query;
 
-        // TODO: filter key presses, don't take Shift, Ctrl, Cmd, Alt into
-        // account, otherwise, extra requests will be sent -> BAD performance
-
-        var query = $(this).val(),
-            url = 'http://' + host + ':' + port + '/?q=' + query;
-
-        if (query != '') {
-            $.get(url, function (result) {
-                if (result != '[]') {
-                    var movies = $.parseJSON(result), list = '';
-                    $(movies).each(function(idx, movie) {
-                        list += '<li>'+ movie.title + ' (' + movie.year + ')</li>';
-                    });
-                    $spinner.hide();
-                    $resultArea.html('<ul>' + list + '</ul>');
-                } else {
-                    $spinner.hide();
-                    $resultArea.html('No hits :(');
-                }
-            });
-        } else {
-            $spinner.hide();
-            $resultArea.html('');
+            if (query != '') {
+                $spinner.show();
+                $.get(url, function (result) {
+                    if (result != '[]') {
+                        var movies = $.parseJSON(result), list = '';
+                        $(movies).each(function(idx, movie) {
+                            list += '<li>'+ movie.title + ' (' + movie.year + ')</li>';
+                        });
+                        $spinner.hide();
+                        $resultArea.html('<ul>' + list + '</ul>');
+                    } else {
+                        $spinner.hide();
+                        $resultArea.html('No hits :(');
+                    }
+                });
+            } else {
+                $spinner.hide();
+                $resultArea.html('');
+            }
         }
     });
 });
